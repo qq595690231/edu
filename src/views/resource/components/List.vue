ac601eb0863940e92303389626bff2fe9d08a66e
WilliamWu <995347291@qq.com>
2020-12-24 12:37:03 +0800

    权限管理-资源分页功能

--- src/views/resource/components/List.vue
+++ src/views/resource/components/List.vue
@@ -3,12 +3,12 @@
     <el-card class="box-card">
       <div slot="header" class="clearfix">
         <!-- 使用 Form 组件：行内表单 -->
-        <el-form :inline="true" :model="formInline" class="demo-form-inline">
+        <el-form :inline="true" :model="form" class="demo-form-inline">
           <el-form-item label="审批人">
-            <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+            <el-input v-model="form.user" placeholder="审批人"></el-input>
           </el-form-item>
           <el-form-item label="活动区域">
-            <el-select v-model="formInline.region" placeholder="活动区域">
+            <el-select v-model="form.region" placeholder="活动区域">
               <el-option label="区域一" value="shanghai"></el-option>
               <el-option label="区域二" value="beijing"></el-option>
             </el-select>
@@ -18,51 +18,59 @@
           </el-form-item>
         </el-form>
       </div>
-      <div>
-        <!-- 使用 Table 组件 -->
-        <el-table
-          :data="resources"
-          style="width: 100%">
-          <el-table-column
-            type="index"
-            label="编号"
-            width="100">
-          </el-table-column>
-          <el-table-column
-            prop="name"
-            label="资源名称">
-          </el-table-column>
-          <el-table-column
-            prop="url"
-            label="资源路径">
-          </el-table-column>
-          <el-table-column
-            prop="description"
-            label="描述">
-          </el-table-column>
-          <!-- 设置过滤器需要使用作用域插槽获取数据 -->
-          <el-table-column
-            label="添加时间">
-            <template slot-scope="scope">
-              <span>{{ scope.row.createdTime | dateFormat }}</span>
-            </template>
-          </el-table-column>
-          <el-table-column
-            label="操作">
-            <template slot-scope="scope">
-              <el-button
-                size="mini"
-                @click="handleEdit(scope.row)"
-              >编辑</el-button>
-              <el-button
-                size="mini"
-                @click="handleDelete(scope.row)"
-                type="danger"
-              >删除</el-button>
-            </template>
-          </el-table-column>
-        </el-table>
-      </div>
+      <!-- 使用 Table 组件 -->
+      <el-table
+        :data="resources"
+        style="width: 100%">
+        <el-table-column
+          type="index"
+          label="编号"
+          width="100">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="资源名称">
+        </el-table-column>
+        <el-table-column
+          prop="url"
+          label="资源路径">
+        </el-table-column>
+        <el-table-column
+          prop="description"
+          label="描述">
+        </el-table-column>
+        <!-- 设置过滤器需要使用作用域插槽获取数据 -->
+        <el-table-column
+          label="添加时间">
+          <template slot-scope="scope">
+            <span>{{ scope.row.createdTime | dateFormat }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="操作">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              @click="handleEdit(scope.row)"
+            >编辑</el-button>
+            <el-button
+              size="mini"
+              @click="handleDelete(scope.row)"
+              type="danger"
+            >删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页组件结构 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="form.current"
+        :page-sizes="[10, 15, 20]"
+        :page-size="form.size"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="totalCount">
+      </el-pagination>
     </el-card>
   </div>
 </template>
@@ -73,19 +81,42 @@ export default {
   name: 'ResourceList',
   data () {
     return {
-      formInline: {},
       // 用于存储资源列表数据
-      resources: []
+      resources: [],
+      form: {
+        // 当前显示的页号
+        current: 1,
+        // 每页显示的数据条数
+        size: 10
+      },
+      // 数据总数
+      totalCount: 0
     }
   },
   created () {
     this.loadResources()
   },
   methods: {
+    // 每页显示条数变化时触发
+    handleSizeChange (val) {
+      this.form.size = val
+      // 由于修改了每页显示的条数，应当将页数还原为默认值 1
+      this.form.current = 1
+      this.loadResources()
+    },
+    // 页号改变触发
+    handleCurrentChange (val) {
+      this.form.current = val
+      this.loadResources()
+    },
     async loadResources () {
-      const { data } = await getResourcePages({})
+      const { data } = await getResourcePages({
+        current: this.form.current,
+        size: this.form.size
+      })
       if (data.code === '000000') {
         this.resources = data.data.records
+        this.totalCount = data.data.total
       }
     },
     handleEdit () {
