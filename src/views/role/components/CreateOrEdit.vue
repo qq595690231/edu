200aa7b9f29b60b6f301f1e065a507b711519927
WilliamWu <995347291@qq.com>
2020-12-25 17:53:50 +0800

    角色与用户功能-编辑角色

--- src/views/role/components/CreateOrEdit.vue
+++ src/views/role/components/CreateOrEdit.vue
@@ -25,9 +25,23 @@
 </template>
 
 <script>
-import { createOrUpdate } from '@/services/role'
+import { createOrUpdate, getRoleById } from '@/services/role'
 export default {
   name: 'CreateOrEdit',
+  props: {
+    isEdit: {
+      type: Boolean,
+      default: false
+    },
+    roleId: {
+      type: [Number, String]
+    }
+  },
+  created () {
+    if (this.isEdit) {
+      this.loadRole()
+    }
+  },
   data () {
     return {
       role: {
@@ -38,6 +52,12 @@ export default {
     }
   },
   methods: {
+    async loadRole () {
+      const { data } = await getRoleById(this.roleId)
+      if (data.code === '000000') {
+        this.role = data.data
+      }
+    },
     onCancel () {
       // 设置取消状态，让父组件处理
       this.$emit('cancel')
