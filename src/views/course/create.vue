f3e2b6870a4a102cd5cd5e53f6efe8b13f067ad2
WilliamWu <995347291@qq.com>
2021-01-04 17:01:11 +0800

    课程管理-添加课程布局

--- src/views/course/create.vue
+++ src/views/course/create.vue
@@ -4,29 +4,126 @@
       <div slot="header">
         <el-steps :active="activeStep" simple>
           <el-step
-            v-for="item in steps"
+            v-for="(item,i) in steps"
             :key="item.id"
             :title="item.title"
             :icon="item.icon"
+            @click.native="activeStep = i"
           ></el-step>
         </el-steps>
       </div>
       <!-- 步骤对应的表单结构 -->
       <el-form>
+        <!-- 基本信息 -->
         <div v-show="activeStep === 0">
-          基本信息
+          <el-form-item label="课程名称">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="课程简介">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="课程概述">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="讲师姓名">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="讲师简介">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="课程排序">
+            <el-input-number
+              label="描述文字"
+              controls-position="right"
+            ></el-input-number>
+          </el-form-item>
         </div>
+        <!-- 课程封面 -->
         <div v-show="activeStep === 1">
-          课程封面
+          <el-form-item label="课程封面">
+            <el-upload
+              class="avatar-uploader"
+              action="https://jsonplaceholder.typicode.com/posts/"
+              :show-file-list="false"
+              :on-success="handleAvatarSuccess"
+              :before-upload="beforeAvatarUpload">
+              <img v-if="imageUrl" :src="imageUrl" class="avatar">
+              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            </el-upload>
+          </el-form-item>
+          <el-form-item label="解锁封面">
+            <el-upload
+              class="avatar-uploader"
+              action="https://jsonplaceholder.typicode.com/posts/"
+              :show-file-list="false"
+              :on-success="handleAvatarSuccess"
+              :before-upload="beforeAvatarUpload">
+              <img v-if="imageUrl" :src="imageUrl" class="avatar">
+              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            </el-upload>
+          </el-form-item>
         </div>
+        <!-- 销售信息 -->
         <div v-show="activeStep === 2">
-          销售信息
+          <el-form-item label="售卖价格">
+            <el-input>
+              <template slot="append">元</template>
+            </el-input>
+          </el-form-item>
+          <el-form-item label="商品原价">
+            <el-input>
+              <template slot="append">元</template>
+            </el-input>
+          </el-form-item>
+          <el-form-item label="销量">
+            <el-input>
+              <template slot="append">单</template>
+            </el-input>
+          </el-form-item>
+          <el-form-item label="活动标签">
+            <el-input></el-input>
+          </el-form-item>
         </div>
+        <!-- 秒杀信息 -->
         <div v-show="activeStep === 3">
-          秒杀信息
+          <el-form-item label="限时秒杀开关" label-width="120px">
+            <el-switch
+              v-model="isSeckill"
+              active-color="#13ce66"
+              inactive-color="#ff4949"
+            >
+            </el-switch>
+          </el-form-item>
+          <template v-if="isSeckill">
+            <el-form-item label="开始时间">
+              <el-date-picker
+                type="datetime"
+                placeholder="选择日期时间">
+              </el-date-picker>
+            </el-form-item>
+            <el-form-item label="结束时间">
+              <el-date-picker
+                type="datetime"
+                placeholder="选择日期时间">
+              </el-date-picker>
+            </el-form-item>
+            <el-form-item label="秒杀价">
+              <el-input>
+                <template slot="append">元</template>
+              </el-input>
+            </el-form-item>
+            <el-form-item label="库存">
+              <el-input>
+                <template slot="append">个</template>
+              </el-input>
+            </el-form-item>
+          </template>
         </div>
+        <!-- 课程详情 -->
         <div v-show="activeStep === 4">
-          课程详情
+          <el-form-item>
+            <el-input type="textarea"></el-input>
+          </el-form-item>
           <el-form-item>
             <el-button
               type="primary"
@@ -58,10 +155,60 @@ export default {
         { id: 3, title: '销售信息', icon: 'el-icon-picture' },
         { id: 4, title: '秒杀信息', icon: 'el-icon-picture' },
         { id: 5, title: '课程详情', icon: 'el-icon-picture' }
-      ]
+      ],
+      // 本地预览图片地址
+      imageUrl: '',
+      // 秒杀状态
+      isSeckill: false
+    }
+  },
+  methods: {
+    // 上传图片成功回调
+    handleAvatarSuccess (res, file) {
+      this.imageUrl = URL.createObjectURL(file.raw)
+    },
+    // 上传前的回调
+    beforeAvatarUpload (file) {
+      const isJPG = file.type === 'image/jpeg'
+      const isLt2M = file.size / 1024 / 1024 < 2
+
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 JPG 格式!')
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 2MB!')
+      }
+      return isJPG && isLt2M
     }
   }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.el-step {
+  cursor: pointer;
+}
+::v-deep .avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+::v-deep .avatar-uploader .el-upload:hover {
+  border-color: #409EFF;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
